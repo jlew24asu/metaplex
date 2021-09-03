060f62c2e3d31396ad336d648b4b55dc1e94163c
Jeff Lewis <jlew24asu@gmail.com>
2021-09-03 13:43:06 -0500

    next-again

--- js/packages/web/next.config.js
+++ js/packages/web/next.config.js
@@ -1,24 +1,25 @@
 
 // next.config.js
-  module.exports = {
-    async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    }
-  };
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
 
-import withPlugins from 'next-compose-plugins';
-import withLess from 'next-with-less';
+
+const withPlugins = require('next-compose-plugins');
+const withLess = require('next-with-less');
 
 const assetPrefix = process.env.ASSET_PREFIX || '';
 
@@ -40,7 +41,7 @@ const plugins = [
   ],
 ];
 
-export default withPlugins(plugins, {
+module.exports = withPlugins(plugins, {
   assetPrefix,
   reactStrictMode: true,
   eslint: {
